6bc0212f2183c36b2c0ca1fc9a22565d2c915a1a
Marjan Jalsovec <jmarjan2@gmail.com>
2025-02-17 22:59:21 +0000

    commit

---
+++ lecture_04/Exercise 2 - Factorial/js/main.js
@@ -0,0 +1,43 @@
+function factorial() { 
+    // declare variables
+    let number = 0;
+    let factorial = 1;
+    let output = "";
+  
+  }
+  
+  factorial();
+  
+  // input
+  number = parseInt(prompt("Enter a number between 1 and 20"));
+  while ( isNaN(number) || number <= 1 || number >= 20) {
+    number = parseInt(prompt("Please enter a number between 1 and 20"));
+  }
+
+  output = "Factorials up to " + number + ": ";
+
+  // calculate factorials
+  for (let j=number; j >= 1; j--) { // outer loop
+    factorial = 1;
+    for (let i=1; i <= j; i++) { // inner loop
+      factorial *= i;
+    }
+    output += j + '! = ' + factorial + ', ';
+  }
+
+  // output factorials
+  alert(output);
+
+  const fac = (num) => {
+    let fac = 1;
+    for (let i=1; i <= num; i++) {
+      fac *= i;
+    }
+    return fac;
+  }
+  //factorial = 1;
+    //for (let i=1; i <= j; i++) { // inner loop
+    //  factorial *= i;
+    //}
+    //output += j + '! = ' + factorial + ', ';
+    output += j + '! = ' + fac(j) + ', ';
--- lecture_04/number-of-stars/js/main.js
+++ lecture_04/number-of-stars/js/main.js
@@ -12,3 +12,10 @@
  
  // output 
  alert("Outputting " + numberOfStars + " stars: " + outputStars)
+
+   // check that user entered a number and that it is greater than zero
+  // otherwise ask user to enter another number
+  while (isNaN(numberOfStars) || numberOfStars <=0) {
+    alert("You did not enter a number greater than zero.");
+    numberOfStars = prompt("Enter a number greater than zero?");
+  }
